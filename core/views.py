d71edc7db5c438e5ce298af5b0c0745fa90bfd0c
Oscann <oscandido10@gmail.com>
2023-09-25 14:23:33 -0300

    pagina de edit

--- core/views.py
+++ core/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.shortcuts import render, redirect
-from django.views.generic import FormView, ListView, DetailView, TemplateView
+from django.views.generic import FormView, ListView, DetailView, TemplateView, UpdateView
 from django.views.generic.edit import DeleteView
 from django.contrib import messages
 from .forms import ReservaModelFrom
@@ -37,3 +37,19 @@ class ReservaDeleteView(DeleteView):
     template_name = 'confirm_delete.html'
     context_object_name = "object"
     success_url = reverse_lazy("reservas")
+    success_message = "Reserva deletada com sucesso! :)"
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super(ReservaDeleteView, self).delete(request, *args, **kwargs)
+
+class ReservaUpdateView(UpdateView):
+    model = Reserva
+    fields = ["cnpj", "nome_empresa", "categoria_empresa", "quitado", "stand"]
+    template_name = "reserva_update_form.html"
+    template_name_suffix = "_update_form"
+    context_object_name = "object"
+
+    def get_success_url(self):
+        messages.success(self.request, "Reserva atualizada com sucesso! :)")
+        return reverse('detail', kwargs={'pk': self.object.pk})
--- django_finecap/urls.py
+++ django_finecap/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import ReservaFormView, ReservaListView, ReservaDetailView, ReservaTemplateView, ReservaDeleteView
+from core.views import ReservaFormView, ReservaListView, ReservaDetailView, ReservaTemplateView, ReservaDeleteView, ReservaUpdateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", ReservaTemplateView.as_view(), name="home"),
-    path("cria_reserva", ReservaFormView.as_view(), name="cria_reserva"),
-    path("reservas", ReservaListView.as_view(), name="reservas"),
-    path("reservas/detail/<pk>", ReservaDetailView.as_view(), name="detail"),
+    path('', ReservaTemplateView.as_view(), name='home'),
+    path('cria_reserva', ReservaFormView.as_view(), name='cria_reserva'),
+    path('reservas', ReservaListView.as_view(), name='reservas'),
+    path('reservas/detail/<pk>', ReservaDetailView.as_view(), name='detail'),
     path('reservas/detail/<pk>/delete', ReservaDeleteView.as_view(), name='reserva_delete'),
+    path("reservas/detail/<pk>/update", ReservaUpdateView.as_view(), name='reserva_update'),
 ]
